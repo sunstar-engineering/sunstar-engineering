188618823f2b3c188b159915b517190fe6f982c6
David Bosschaert <bosschae@adobe.com>
2023-06-01 10:03:56 +0100

    wip-testing

--- test/blocks/news/news.test.js
+++ test/blocks/news/news.test.js
@@ -2,26 +2,38 @@
 /* global describe it */
 
 import { expect } from '@esm-bundle/chai';
-import { getLatestNewsArticleInt } from '../../../blocks/news/news.js';
+import { readFile } from '@web/test-runner-commands';
+import decorate from '../../../blocks/news/news.js';
+
+document.write(await readFile({ path: './news.plain.html' }));
+
+const block = document.querySelector('.news');
+await decorate(block);
 
 describe('News Block', () => {
-  it('Latest news article', async () => {
-    let idxName = '';
-    const ffunc = (nm) => {
-      idxName = nm;
 
-      return {
-        data: [
-          { path: '/news/b/', title: 'b', lastModified: 1685443972 },
-          { path: '/news/a/', title: 'a', lastModified: 1685443971 },
-          { path: '/c/news/', title: 'c', lastModified: 1685443970 },
-        ],
-      };
-    };
-
-    const latest = await getLatestNewsArticleInt(ffunc);
-
-    expect(idxName).to.equal('query-index');
-    expect(latest.title).to.equal('a');
-  });
 });
+
+// import { getLatestNewsArticleInt } from '../../../blocks/news/news.js';
+
+// describe('News Block', () => {
+//   it('Latest news article', async () => {
+//     let idxName = '';
+//     const ffunc = (nm) => {
+//       idxName = nm;
+
+//       return {
+//         data: [
+//           { path: '/news/b/', title: 'b', lastModified: 1685443972 },
+//           { path: '/news/a/', title: 'a', lastModified: 1685443971 },
+//           { path: '/c/news/', title: 'c', lastModified: 1685443970 },
+//         ],
+//       };
+//     };
+
+//     const latest = await getLatestNewsArticleInt(ffunc);
+
+//     expect(idxName).to.equal('query-index');
+//     expect(latest.title).to.equal('a');
+//   });
+// });
